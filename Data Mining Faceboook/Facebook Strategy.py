dd3b1b7e3e3a129aefa411c58d6fe3b737553ec1
ArecRui <createrliang@gmail.com>
2016-04-10 14:09:47 +0800

    Add counting events

--- Data Mining Faceboook/Facebook Strategy.py
+++ Data Mining Faceboook/Facebook Strategy.py
@@ -17,13 +17,13 @@ def access_pages(id_lst, url_prefix, access_token):
     param_page = {"access_token": access_token, "fields": "name,id,phone,emails,likes,events,posts{type,created_time}"}
     #print('make page param')
     time_range = ['0','0']
-    time_range[0] = input("Please enter start date(yyyymmdd). For example 20140101:")
-    time_range[1] = input("Please enter end date(yyyymmdd). For example 20150101:")
+    time_range[0] = '2011-01-01'#input("Please enter start date(yyyymmdd). For example 20140101:")
+    time_range[1] = '2020-01-01'#input("Please enter end date(yyyymmdd). For example 20150101:")
     for page_id in id_lst:
         tmp_page_info = {'page_title':'','percnt_text':0,'percnt_video':0,'percnt_photo':0,'percnt_link':0,'percnt_other':0,'phone':'','emails':'','likes':'','online':0}
         tmp_post_info = {'averlikes':0,'avercomts':0,'avershares':0,'percorig':0,'percrepost':0,'responseto':0,'postscount':0}
-        tmp_type_counts = {"link": 0, "status": 0, "photo": 0, "video":0, "offer": 0,"note":0}
-        tmp_postattri_info = {'tmp_sharescount_list':[],'tmp_likecount_list':[],'tmp_comentcount_list':[],'tmptype':'','repostcount':0}
+        tmp_type_counts = {"link": 0, "status": 0, "photo": 0, "video":0, "offer": 0,"note":0,"event":0}
+        tmp_postattri_info = {'tmp_sharescount_list':[],'tmp_likecount_list':[],'tmp_comentcount_list':[],'tmp_onlinecount_list':[],'tmptype':'','repostcount':0}
         tmp_posts_counts = 0
         r = requests.get(url_prefix+page_id, params=param_page)
         if r.headers.get('Facebook-API-Version') == None:
@@ -44,7 +44,7 @@ def access_pages(id_lst, url_prefix, access_token):
         else:
             tmp_page_info['emails'] = 0
         tmp_page_info['likes'] = str(r_json.get('likes'))
-        tmp_page_info['online'] = str(r_json.get('events'))
+        tmp_page_info['online'] = events_count(page_id, url_prefix, access_token)               #str(r_json.get('events'))
         while (page_json.get('paging')== None) == False:
             posts = page_json.get('data')
             for post in posts:
@@ -102,7 +102,7 @@ def access_pages(id_lst, url_prefix, access_token):
         else:
             print("tmp_postattri_info['tmp_comentcount_list']", tmp_postattri_info['tmp_comentcount_list'])
             tmp_post_info['avercomts'] = sum(tmp_postattri_info['tmp_comentcount_list'])/len(tmp_postattri_info['tmp_comentcount_list'])           
-            
+        
         tmp_page_info['percnt_text'] = tmp_type_counts['status']/tmp_posts_counts
         tmp_page_info['percnt_video'] = tmp_type_counts['video']/tmp_posts_counts
         tmp_page_info['percnt_photo'] = tmp_type_counts['photo']/tmp_posts_counts
@@ -146,6 +146,36 @@ def access_posts(post_id, url_prefix, access_token,tmp_postattri_info):
     #print(post_type)
     tmp_postattri_info['tmptype'] = post_type
     return tmp_postattri_info
+
+def events_count(page_id, url_prefix, access_token):
+    print ('counting events number')
+    events_param = {"access_token": access_token, "fields": "events{limit=100000000}"}
+    r = requests.get(url_prefix+page_id, params=events_param)
+    if r.headers.get('Facebook-API-Version') == None:
+        access_token[0] = input("Access token expired. Please enter a new access token: ")
+        r = requests.get(url_prefix+page_id, params=events_param)
+    r_json =  r.json()
+    page_json = r_json.get('events')
+    events = []
+    while True:
+        if (page_json == None):
+            break
+        elif page_json.get('data') == None:
+            break
+        else:
+            events = events + page_json.get('data')
+        if page_json.get('paging').get('next') == None:
+            break
+        else:
+            nextpageurl = page_json.get('paging').get('next')
+            #print('get url of next page', nextpageurl)
+            nextpage = requests.get(nextpageurl)
+            #print('get data of next page of posts', nextpage)
+            page_json = nextpage.json()
+            #print('jsonlize next page', page_json)
+    number = len(events)
+    return number
+            
 
 def likecount (post_id, url_prefix, access_token):
     like_param = {"access_token": access_token, "fields": "likes{limit=100000000}"}
@@ -217,8 +247,7 @@ if __name__ == '__main__':
     f_write.write('Start Time: ' + str(start_time) + '\n')
     print('read id list')
     print('start access pages')
-    access_token = ['0']
-    access_token[0] = input("Please enter access token for facebook api: ")
+    access_token = ['CAACEdEose0cBAF0gNFbmaP4ZCypEXMQ7hXZAGHSIzeVZAc46NzxLwA9OydMVZAW138Ktz6Bx6GoKZBkW1BZADIzKAUZCnjdmeAR4y3ojDZAtusQMZBOjovzFtlO26MjbruPl6BXWYttwnKw4wWSLNYk9sFAiejaoh0KZAPmS6C7NcIxZAYFTklbAVJVf0WfpWzpU91JZAtrMLa1nxFB2oJm7vdah']
     print("get access_token")
     access_pages(id_lst, 'https://graph.facebook.com/v2.5/', access_token)
     f_write = open('page_stas_output.txt', 'a')
